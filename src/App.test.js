e4bbde7924155de42fe2b2b984c9dae30703197e
daniyalnagori <daniyalnagori@yahoo.com>
2020-08-04 22:44:32 +0500

    added .travis file

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
+test('text case', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello Piaic/i);
   expect(linkElement).toBeInTheDocument();
 });
